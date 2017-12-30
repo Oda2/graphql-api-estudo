4cb71a2e77db5c8c3cb346efa6d78eacd6aa0b42
Renato Oda <renato.oda@onclicksistemas.com.br>
2017-12-30 12:59:56 -0200

    Tarefas com gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,30 @@ const ts = require('gulp-typescript');
 
 const tsProject = ts.createProject('tsconfig.json');
 
-gulp.task('scripts', () => {
+gulp.task('scripts', ['static'], () => {
   const tsResult = tsProject.src()
     .pipe(tsProject());
 
   return tsResult.js
     .pipe(gulp.dest('dist'));
 });
+
+gulp.task('static', ['clean'], () => {
+  return gulp
+    .src(['src/**/*.json'])
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', () => {
+  return gulp
+    .src('dist')
+    .pipe(clean());
+});
+
+gulp.task('watch', ['build'], () => {
+  return gulp.watch(['src/**/*.ts', 'src/**/*.json'], ['build']);
+});
+
+gulp.task('build', ['clean', 'static', 'scripts']);
+
+gulp.task('default', ['watch']);
