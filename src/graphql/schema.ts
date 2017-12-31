b922f99372b4d37c120a012d65be0ac489862ce9
Renato Oda <renato.oda2@gmail.com>
2017-12-31 16:33:20 -0200

    Resolvers Trivial

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -30,6 +30,11 @@ const typeDefs = `
 `;
 
 const resolvers = {
+  User: { // Resolver trivial
+    id: (user) => user.id,
+    name: (user) => user.name,
+    email: (user) => user.email
+  },
   Query: {
     allUsers: () => users
   },
