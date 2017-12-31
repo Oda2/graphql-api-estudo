32df04ff1da5f6adbf867586a76d2947e1310276
Renato Oda <renato.oda2@gmail.com>
2017-12-31 16:22:45 -0200

    Habilitando interface do graphql

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,8 @@ class App {
 
   private middleware(): void {
     this.express.use('/graphql', graphqlHTTP({
-      schema: schema
+      schema: schema,
+      graphiql: process.env.NODE_ENV === 'development'
     }));
   }
 }
