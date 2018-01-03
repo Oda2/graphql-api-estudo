2f2d239c925decb7b0d268bd2d21da5621191b13
Renato Oda <renato.oda2@gmail.com>
2018-01-03 17:27:13 -0200

    Ajuste do Post Resolvers

--- src/graphql/resources/post/post.resolvers.ts
+++ src/graphql/resources/post/post.resolvers.ts
@@ -5,6 +5,23 @@ import { DbConnection } from "../../../interfaces/DbConnectionInterface";
 import { PostInstance } from "../../../models/PostModel";
 
 export const postResolvers = {
+
+  Post: {
+    author: (parent, args, { db }: { db: DbConnection }, info: GraphQLResolveInfo) => {
+      return db.User
+        .findById(parent.get('author'));
+    },
+
+    comment: (parent, { first = 10, offset = 0 }, { db }: { db: DbConnection }, info: GraphQLResolveInfo) => {
+      return db.Comment
+        .findAll({
+          where: { post: parent.get('id') },
+          limit: first,
+          offset: offset
+        });
+    }
+  },
+
   Query: {
     posts: (parent, { first = 10, offset = 0 }, { db }: { db: DbConnection }, info: GraphQLResolveInfo) => {
       return db.Post
