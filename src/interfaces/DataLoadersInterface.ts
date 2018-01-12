b794c4655f6059e499bec0e2a9daf3ad27f1e129
Renato Oda <renato.oda2@gmail.com>
2018-01-12 11:18:30 -0200

    Interface DataLoader

---
+++ src/interfaces/DataLoadersInterface.ts
@@ -0,0 +1,9 @@
+import * as DataLoader from 'dataloader';
+
+import { UserInstance } from '../models/UserModel';
+import { PostInstance } from '../models/PostModel';
+
+export interface DataLoaders {
+  userLoader: DataLoader<number, UserInstance>;
+  postLoader: DataLoader<number, PostInstance>;
+}
