232f4b122fbed4df4aebd66237765f9e1d01be8f
Renato Oda <renato.oda2@gmail.com>
2018-01-12 18:26:21 -0200

    Criação do primeiro teste

--- src/models/index.ts
+++ src/models/index.ts
@@ -29,7 +29,8 @@ if (!db) {
   fs
     .readdirSync(__dirname)
     .filter((file: string) => {
-      return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+      const fileSlice: string = file.slice(-3)
+      return (file.indexOf('.') !== 0) && (file !== basename) && ((fileSlice === '.js') || (fileSlice === '.ts'));
     })
     .forEach((file: string) => {
       const model = sequelize.import(path.join(__dirname, file));
