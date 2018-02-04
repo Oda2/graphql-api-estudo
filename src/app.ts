ef76a42f390d3cd2dd35d2a4a0daf7dea80a60d1
Renato Oda <renato.oda2@gmail.com>
2018-02-04 21:22:48 -0200

    Habilitando o helmet

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import * as express from 'express';
 import * as graphqlHTTP from 'express-graphql';
 import * as cors from 'cors';
 import * as compression from 'compression';
+import * as helmet from 'helmet';
 
 import db from './models';
 import schema from './graphql/schema';
@@ -35,6 +36,8 @@ class App {
     }));
 
     this.express.use(compression());
+
+    this.express.use(helmet());
     
     this.express.use('/graphql',
       (req, res, next) => {
